05c71ac4ef0e1faaff77b7e9eccb3d13b40b499a
shwetap29 <shwetaprasad48@gmail.com>
2021-07-02 07:44:27 +0530

    ADDED:Calculated Wages For A month

--- UC4_wagesForAMonth.js
+++ UC4_wagesForAMonth.js
@@ -1,11 +1,11 @@
-// UC 4 - Calculating Wages for a Month 
+// Calculate Wages For A month
 
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20; 
-const NUM_OF_WORKING_DAYS = 2;
+const NUM_OF_WORKING_DAYS = 20;
 
 function getWorkingHours(empCheck) {
     switch(empCheck){
