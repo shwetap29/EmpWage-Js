879ee8b95886ea6d62d1a76b0299472c06d6d320
shwetap29 <shwetaprasad48@gmail.com>
2021-07-02 08:45:00 +0530

    ADDED:Stored Daily Wage Along With Total wage

---
+++ UC6_dailyWageAlongWTotalWage.js
@@ -0,0 +1,40 @@
+// Store Daily  Wage Along with Total Wage 
+
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20; 
+const NUM_OF_WORKING_DAYS = 20;
+const MAX_HRS_IN_MONTH = 160;
+
+function getWorkingHours(empCheck) {
+    switch(empCheck){
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        default:
+            return 0;
+    }
+}
+
+function calcWage(empHrs) {
+    return empHrs * WAGE_PER_HOUR;
+}
+
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+let empWageArray = new Array();
+
+while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+    totalWorkingDays++;
+    let empCheck = Math.floor((Math.random() * 10) % 3);
+    let empHrs = getWorkingHours(empCheck);
+    totalEmpHrs += empHrs;
+    empWageArray.push(calcWage(empHrs)); 
+}
+
+let empWage = calcWage(totalEmpHrs);
+
+console.log("Total Employee Working Hours : " + totalEmpHrs + "\nTotal Employee Working Days :  " + totalWorkingDays + "\nTotal Employee Wage : " + empWage);
